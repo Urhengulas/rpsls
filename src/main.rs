5297ec8d4306ac94dbaf8603728691093ddc41f2
Urhengulas <johann.hemmann@code.berlin>
2022-07-31 02:20:06 +0200

    Create Game scaffold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let game = Game::new();
+    game.start()
+}
+
+struct Game {}
+
+impl Game {
+    fn new() -> Game {
+        Game {}
+    }
+
+    fn start(&self) {
+        print!("[0] PVP\n[1] PCV\n[2] Exit\n>>> ");
+        io::stdout().flush().unwrap();
+
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+
+        match buf.as_str() {
+            "0" => {}
+            "1" => {}
+            "2" => {}
+            _ => {}
+        }
+    }
 }
